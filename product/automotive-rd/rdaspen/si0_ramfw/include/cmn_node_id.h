49f701043c6abe4dca42f503dedabdef6ecbb206
Peter Hoyes <peter.hoyes@arm.com>
2025-09-03 11:10:13 +0100

    prod/rdaspen: Map non-hash DRAM regions
    The DRAM accesses are not hash; they are striped according
    to specific ranges.
    
    Add these ranges to the rdaspen CMN configuration. Remove the explicit
    "power-of-two" hash configuration as no hash regions are now used.
    
    Reorder according to DMC ordering.
    
    Upstream-Status: Pending
    Signed-off-by: Peter Hoyes <peter.hoyes@arm.com>

--- product/automotive-rd/rdaspen/si0_ramfw/include/cmn_node_id.h
+++ product/automotive-rd/rdaspen/si0_ramfw/include/cmn_node_id.h
@@ -18,14 +18,14 @@
 #define NODE_ID_HND  4 /* System Control Block */
 
 #define NODE_ID_SBSX0 0
-#define NODE_ID_SBSX1 64
-#define NODE_ID_SBSX2 128
-#define NODE_ID_SBSX3 192
-#define NODE_ID_SBSX4 256
-#define NODE_ID_SBSX5 88
-#define NODE_ID_SBSX6 152
-#define NODE_ID_SBSX7 216
-#define NODE_ID_SBSX8 280
+#define NODE_ID_SBSX1 88
+#define NODE_ID_SBSX2 152
+#define NODE_ID_SBSX3 216
+#define NODE_ID_SBSX4 280
+#define NODE_ID_SBSX5 64
+#define NODE_ID_SBSX6 128
+#define NODE_ID_SBSX7 192
+#define NODE_ID_SBSX8 256
 
 #define MESH_SIZE_X 6
 #define MESH_SIZE_Y 4
