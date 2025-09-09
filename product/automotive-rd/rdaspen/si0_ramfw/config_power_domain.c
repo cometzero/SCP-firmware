b31015757d592335bad12a8bdb1b6a207bd7e147
Divin Raj <divin.raj@arm.com>
2025-09-09 10:14:16 +0100

    product/rdaspen: Add power domain config for SI CL1
    Add power domain configuration for Safety Island Cluster 1
    (SI CL1).
    
    Upstream-Status: Pending
    
    Signed-off-by: Divin Raj <divin.raj@arm.com>

--- product/automotive-rd/rdaspen/si0_ramfw/config_power_domain.c
+++ product/automotive-rd/rdaspen/si0_ramfw/config_power_domain.c
@@ -10,6 +10,7 @@
 
 #include "platform_core.h"
 #include "si0_cfgd_power_domain.h"
+#include "si_scr_info.h"
 
 #include <power_domain_utils.h>
 
@@ -58,10 +59,67 @@ static const struct mod_power_domain_config platform_power_domain_config = {
     0
 };
 
+/* Create SI Cluster1 power domain elements */
+static int create_si_cluster_elements(
+    struct fwk_element *si_dst,
+    unsigned int dst_base_idx,
+    unsigned int dst_capacity)
+{
+    struct fwk_element dummy_static_table = { 0 };
+    const struct fwk_element *si_src = NULL;
+    struct mod_power_domain_element_config *pd_config;
+
+    if (si_dst == NULL)
+        return FWK_E_PARAM;
+
+    if (SI1_TOTAL_ELEMENTS > dst_capacity)
+        return FWK_E_RANGE;
+
+    si_src = create_power_domain_element_table(
+        SI1_CORE_COUNT,
+        SI1_CLUSTER_COUNT,
+        FWK_MODULE_IDX_PPU_V1,
+        MOD_PPU_V1_API_IDX_POWER_DOMAIN_DRIVER,
+        core_pd_allowed_state_mask_table,
+        FWK_ARRAY_SIZE(core_pd_allowed_state_mask_table),
+        cluster_pd_allowed_state_mask_table,
+        FWK_ARRAY_SIZE(cluster_pd_allowed_state_mask_table),
+        &dummy_static_table,
+        0);
+
+    if (si_src == NULL)
+        return FWK_E_NOMEM;
+
+    fwk_str_memcpy(
+        si_dst, si_src, SI1_TOTAL_ELEMENTS * sizeof(struct fwk_element));
+
+    for (unsigned int i = 0; i < SI1_TOTAL_ELEMENTS; ++i) {
+        pd_config = (struct mod_power_domain_element_config *)si_dst[i].data;
+        if (i < SI1_CORE_COUNT) {
+            pd_config->parent_idx = dst_base_idx + SI1_CORE_COUNT;
+        } else {
+            pd_config->parent_idx = PD_STATIC_DEV_IDX_NONE;
+        }
+
+        pd_config->driver_id =
+            FWK_ID_ELEMENT(FWK_MODULE_IDX_PPU_V1, dst_base_idx + i);
+    }
+
+    return FWK_SUCCESS;
+}
+
 static const struct fwk_element *platform_power_domain_get_element_table(
     fwk_id_t module_id)
 {
     struct mod_power_domain_element_config *systop_pd_config;
+    const struct fwk_element *systop_elements = NULL;
+    const struct fwk_element *final_elements = NULL;
+    struct fwk_element *all_elements = NULL;
+    struct fwk_element *si_dst = NULL;
+    unsigned int systop_count;
+    unsigned int total_count;
+    unsigned int si_capacity;
+    int st;
 
     systop_pd_config =
         fwk_mm_calloc(1, sizeof(struct mod_power_domain_element_config));
@@ -83,7 +141,7 @@ static const struct fwk_element *platform_power_domain_get_element_table(
     };
 
     /* Create power doamin elements for SYSTOP */
-    return create_power_domain_element_table(
+    systop_elements = create_power_domain_element_table(
         platform_get_core_count(),
         platform_get_cluster_count(),
         FWK_MODULE_IDX_PPU_V1,
@@ -94,6 +152,40 @@ static const struct fwk_element *platform_power_domain_get_element_table(
         FWK_ARRAY_SIZE(cluster_pd_allowed_state_mask_table),
         pd_static_element_table,
         FWK_ARRAY_SIZE(pd_static_element_table));
+
+    if (systop_elements == NULL) {
+        return NULL;
+    }
+    final_elements = systop_elements;
+
+    if (si_cl1_present()) {
+        /* Create SI_Cluster1 elements */
+        systop_count = platform_get_core_count() +
+            platform_get_cluster_count() +
+            FWK_ARRAY_SIZE(pd_static_element_table);
+        total_count = systop_count + SI1_TOTAL_ELEMENTS;
+
+        /* +1 for the final terminator */
+        all_elements =
+            fwk_mm_calloc(total_count + 1, sizeof(struct fwk_element));
+
+        fwk_str_memcpy(
+            all_elements,
+            systop_elements,
+            systop_count * sizeof(struct fwk_element));
+
+        /* Fill SI block */
+        si_dst = all_elements + systop_count;
+        si_capacity = total_count - systop_count;
+
+        st = create_si_cluster_elements(si_dst, systop_count, si_capacity);
+        if (st != FWK_SUCCESS)
+            return NULL;
+
+        final_elements = all_elements;
+    }
+
+    return final_elements;
 }
 
 const struct fwk_module_config config_power_domain = {
